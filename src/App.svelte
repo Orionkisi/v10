245af7f12f3dde36cdddb022d5420afa02f294b3
Hjörtur Sigurðsson <Orionkisi@gmail.com>
2024-11-11 23:52:10 +0000

    awawdawd

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Trivia from './Trivia.svelte';
+</script>
+
+<main class="bg-gray-100 min-h-screen flex items-center justify-center">
+  <Trivia />
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{svelte,js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
